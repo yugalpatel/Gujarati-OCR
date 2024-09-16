f7c755e504201ddc334607fbf3f92b534c3b4628
Yugal Patel <yugalpatel9330@gmail.com>
2024-09-15 21:16:55 -0400

    Updated imports for production

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask
+from routes import bp as routes_bp
 
 def create_app():
     app = Flask(__name__)
     app.config['UPLOAD_FOLDER'] = 'static/uploads'
     
-    from . import routes
-    app.register_blueprint(routes.bp)
+    app.register_blueprint(routes_bp)
     
     return app
 
-# Initialize the app
 app = create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
+
